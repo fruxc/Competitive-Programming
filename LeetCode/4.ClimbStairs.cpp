4485b50344639671c21bb60a8c61bcead8654a64
Hammad <hammadansari17@gmail.com>
2022-07-06 20:59:33 +0530

    climb stairs

---
+++ LeetCode/4.ClimbStairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int climbStairs(int n)
+{
+    int dp[n + 1];
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = dp[i - 1];
+        if (i >= 2)
+        {
+            dp[i] += dp[i - 2];
+        }
+    }
+    return dp[n];
+}
+int main()
+{
+    cout << climbStairs(4) << endl;
+    return 0;
+}
